d73ba6a1fc7dd7a6979da7399b5d3aa663404cea
Richard Astbury <richard.astbury@gmail.com>
2017-11-17 13:30:22 +0000

    initial commit

---
+++ BulkInsertTests/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using Dapper;
+
+namespace BulkInsertTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection("Data Source=localhost;Initial Catalog=Test;Trusted_Connection=Yes;"))
+            {
+                var tester = new Tester(connection, 10_000);
+
+                var run = new Action<string,Action>((name, test) => { 
+
+                    connection.Execute("truncate table [test]");
+                    var stopwatch = Stopwatch.StartNew();
+
+                    test();
+
+                    stopwatch.Stop();
+
+                    Console.WriteLine($"{name} - elapsed time: {stopwatch.ElapsedMilliseconds}ms");
+                });
+
+                run("BulkInsert", tester.BulkInsert);
+                run("OneByOne", tester.OneByOne);
+                run("BatchOf1000", tester.BatchOf1000);
+            }
+
+            Console.WriteLine("Press [Enter]...");
+            Console.ReadLine();
+        }
+
+
+        
+
+    }
+}
+
+
+/*
+OneByOne    - elapsed time: 54533ms (10,000 records) 
+BulkInsert  - elapsed time:  9315ms (1,000,000 records)
+BatchOf1000 - elapsed time: 22256ms (1,000,000 records)
+*/
+
---
+++ BulkInsertTests/Tester.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace BulkInsertTests
+{
+    class Tester
+    {
+        SqlConnection connection;
+        int count;
+        public Tester(SqlConnection connection, int count)
+        {
+            this.connection = connection;
+            connection.Open();
+            this.count = count;
+        }
+
+        public void OneByOne()
+        {
+            const string sql = "INSERT INTO [Test] ([Value]) Values (@Value)";
+            for (var i = 0; i < count; i++)
+            {
+                connection.Execute(sql, new { Value = Guid.NewGuid().ToString()});
+            }
+        }
+
+        public void BatchOf1000()
+        {
+            foreach (var batch in Enumerable.Range(0, count).Chunk(1000))
+            {
+                if (batch.Length == 0) continue;
+                var sql = "INSERT INTO [Test] ([Value]) VALUES \r\n" + string.Join(",\r\n", batch.Select(x => $"('{Guid.NewGuid().ToString()}')"));
+                connection.Execute(sql);
+            }
+        }
+
+        public void BulkInsert()
+        {
+            var table = new DataTable();
+            table.Columns.Add("Value", typeof(string));
+
+            for (var i = 0; i < count; i++)
+            {
+                table.Rows.Add(Guid.NewGuid().ToString());
+            }
+
+            using (var bulk = new SqlBulkCopy(this.connection))
+            {
+                bulk.DestinationTableName = "test";
+                bulk.WriteToServer(table);
+            }
+        }
+
+    }
+
+
+    public static class Extensions
+    {
+
+        public static IEnumerable<T[]> Chunk<T>(this IEnumerable<T> values, int chunkSize)
+        {
+            var buffer = new List<T>(chunkSize);
+
+            foreach (var item in values)
+            {
+                buffer.Add(item);
+                if (buffer.Count >= chunkSize)
+                {
+                    yield return buffer.ToArray();
+                    buffer.Clear();
+                }
+            }
+
+            yield return buffer.ToArray();
+
+        }
+
+    }
+}
